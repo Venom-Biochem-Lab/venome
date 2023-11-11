afaa1d253361b9237b10fd4152d7cdcbfd86fb7d
xnought <bertuccd@oregonstate.edu>
2023-11-11 10:57:12 -0800

    feat: frontend display single protein entry given id

--- frontend/src/routes/protein/[proteinId]/+page.svelte
+++ frontend/src/routes/protein/[proteinId]/+page.svelte
@@ -1,25 +1,35 @@
 <script lang="ts">
 	import { onMount } from "svelte";
-	import { Backend } from "$lib/backend";
+	import { Backend, type ProteinEntry } from "$lib/backend";
 
 	export let data; // linked to +page.ts return (aka the id)
-	let entry: any;
+	let entry: ProteinEntry | null = null;
+	let error = false;
 
 	// when this component mounts, request protein wikipedia entry from backend
 	onMount(async () => {
 		// Request the protein from backend given ID
 		console.log("Requesting", data.proteinId, "info from backend");
+
 		entry = await Backend.getProteinEntry(data.proteinId);
+		// if we could not find the entry, the id is garbo
+		if (entry == null) error = true;
+
 		console.log("Received", entry);
 	});
 </script>
 
 {#if entry}
 	<!-- if got entry from backend, display it -->
-	<h1>{entry}</h1>
-{:else}
-	<!-- Otherwise, tell user we have an error -->
-	<h1 style="color: red;">Error</h1>
+	<h1>{entry.name}</h1>
+	<p>ID: {entry.id}</p>
+{:else if !error}
+	<!-- Otherwise, tell user we tell the user we are loading -->
+	<h1>Loading...</h1>
+{:else if error}
+	<!-- if we error out, tell the user the id is shiza -->
+	<h1>Error</h1>
+	<p>Could not find a protein with the id <code>{data.proteinId}</code></p>
 {/if}
 
 <style>
