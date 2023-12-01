35dd5f042d3d9788ee5ad3fe01da52128f2fcd1d
xnought <bertuccd@oregonstate.edu>
2023-12-01 14:31:41 -0800

    feat: auto go to the search url

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
-	/* Put stuff here */
+	import { goto } from "$app/navigation";
+	import { onMount } from "svelte";
+
+	// remove once we have a landing page
+	onMount(() => {
+		goto("/search");
+	});
 </script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
 
 <div>Landing Page</div>
 
