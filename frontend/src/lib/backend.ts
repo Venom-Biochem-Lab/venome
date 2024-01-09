82d3f59d7827c43c8d50dcebd160fb90264a23f9
xnought <bertuccd@oregonstate.edu>
2024-01-09 15:45:04 -0800

    put the frontend outside of docker container

--- frontend/src/lib/backend.ts
+++ frontend/src/lib/backend.ts
@@ -3,6 +3,7 @@ export { DefaultService as Backend } from "../openapi";
 import { OpenAPI } from "../openapi";
 import { env } from "$env/dynamic/public";
 
-export const BACKEND_URL = env["PUBLIC_BACKEND_URL"];
+// export const BACKEND_URL = env["PUBLIC_BACKEND_URL"];
+export const BACKEND_URL = "http://localhost:8000";
 if (!BACKEND_URL) throw new Error("PUBLIC_BACKEND_URL is not set in .env");
 OpenAPI.BASE = BACKEND_URL;
