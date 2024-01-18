a90fb93cf3a139e11332b35c374b65bc37dd31c0
xnought <bertuccd@oregonstate.edu>
2024-01-17 17:59:42 -0800

    feat: add basic login page to frontend with no logic

---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { Button, Label, Input } from "flowbite-svelte";
+
+	let username: string = "";
+	let password: string = "";
+</script>
+
+<form on:submit|preventDefault class="flex gap-4 flex-col">
+	<div>
+		<Label for="username">Username</Label>
+		<Input
+			id="username"
+			bind:value={username}
+			placeholder="Enter your username..."
+		/>
+	</div>
+
+	<div>
+		<Label for="password">Password</Label>
+		<Input
+			type="password"
+			id="password"
+			bind:value={password}
+			placeholder="Enter your password..."
+		/>
+	</div>
+
+	<Button
+		type="submit"
+		on:click={() => {
+			console.log("hit submit", password, username);
+		}}>Login</Button
+	>
+</form>
