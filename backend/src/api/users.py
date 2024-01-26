fd05ef8ca145fb519df5f418c84ac7562f382331
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2024-01-26 02:49:57 -0800

    Ran Ruff linter to make code look better

--- backend/src/api/users.py
+++ backend/src/api/users.py
@@ -1,11 +1,12 @@
 ﻿from fastapi import APIRouter
 import logging as log
 from passlib.hash import bcrypt
-from ..api_types import  LoginBody, LoginError, ResponseToken
+from ..api_types import LoginBody, LoginError, ResponseToken
 from ..db import Database
 from ..auth import generateAuthToken
 
 router = APIRouter()
+
 
 @router.post("/users/login", response_model=ResponseToken | LoginError)
 def login(body: LoginBody):
@@ -14,24 +15,26 @@ def login(body: LoginBody):
             email = body.email
             password = body.password
 
-            query = """SELECT users.pword, users.admin FROM users WHERE users.email = %s;"""
+            query = (
+                """SELECT users.pword, users.admin FROM users WHERE users.email = %s;"""
+            )
             entry_sql = db.execute_return(query, [email])
 
             # Returns "incorrect email/password" message if there is no such account.
             if entry_sql is None or len(entry_sql) == 0:
                 return LoginError.INCORRECT
-            
+
             # Grabs the stored hash and admin.
             password_hash, admin = entry_sql[0]
 
             # Returns "incorrect email/password" message if password is incorrect.
             if not bcrypt.verify(password, password_hash):
                 return LoginError.INCORRECT
-            
+
             # Generates the token and returns
             token = generateAuthToken(email, admin)
             return ResponseToken(token=token)
-                
+
         except Exception as e:
             log.error(e)
             # TODO: Return something better than query error
--- backend/src/auth.py
+++ backend/src/auth.py
@@ -1,25 +1,27 @@
 ﻿import jwt
 from datetime import datetime, timezone, timedelta
 
-#TODO: This method of secret key generation is, obviously, extremely unsafe.
+# TODO: This method of secret key generation is, obviously, extremely unsafe.
 # This needs to be changed.
 secret_key = "SuperSecret"
+
 
 def generateAuthToken(userId, admin):
     payload = {
         "email": userId,
         "admin": admin,
-        "exp": datetime.now(tz=timezone.utc) + timedelta(hours=24)
+        "exp": datetime.now(tz=timezone.utc) + timedelta(hours=24),
     }
     return jwt.encode(payload, secret_key, algorithm="HS256")
 
-#TODO: Find out how FastAPI handles middleware functions, and turn this into one.
+
+# TODO: Find out how FastAPI handles middleware functions, and turn this into one.
 def authenticateToken(token):
     # Return the decoded token if it's valid.
     try:
         decoded = jwt.decode(token, secret_key, algorithms="HS256")
         return decoded
-    
+
     # If the token is invalid, return None.
     except Exception:
         return None
