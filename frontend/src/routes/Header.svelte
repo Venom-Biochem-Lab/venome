4b594590cfa407cea54dd9b9cfd064d9e7bdd543
xnought <bertuccd@oregonstate.edu>
2024-01-25 20:04:42 -0800

    feat: add new color scheme and font

--- frontend/src/routes/Header.svelte
+++ frontend/src/routes/Header.svelte
@@ -1,26 +1,18 @@
 <script lang="ts">
 	import logo from "$lib/images/logo.svg";
-	import github from "$lib/images/github.svg";
 </script>
 
 <header>
 	<div class="nav-container">
-		<div class="corner">
+		<div id="logo">
 			<a href="/">
 				<img src={logo} alt="Venome Logo" />
 			</a>
 		</div>
 		<div class="nav">
-			<a href="/search">Search</a>
+			<a href="/">Home</a>
 			<a href="/upload">Upload</a>
-			<a href="/login">Login</a>
 		</div>
-	</div>
-
-	<div class="corner">
-		<a href="https://github.com/xnought/venome">
-			<img src={github} alt="GitHub" />
-		</a>
 	</div>
 </header>
 
@@ -28,43 +20,37 @@
 	header {
 		display: flex;
 		justify-content: space-between;
-		background-color: hsla(15, 98%, 51%, 0.06);
-		box-shadow: 0px 0px 3px 2px hsla(0, 0%, 0%, 0.208);
+		background-color: #08163803;
+		box-shadow: 0px 0px 2px 2px hsla(0, 0%, 0%, 0.1);
 		height: 60px;
+	}
+
+	#logo {
+		/* TODO remove these hard coded constraints and do it right */
+		margin-top: 10px;
+		height: 45px;
+		width: 100px;
 	}
 
 	.nav-container {
 		display: flex;
 		height: 60px;
-		gap: 10px;
+		gap: 20px;
+		margin-left: 20px;
 	}
 
 	.nav {
 		display: flex;
 		align-items: center;
-		gap: 10px;
+		gap: 15px;
+		font-size: 18px;
+		font-weight: 300;
 	}
 
-	.corner {
-		width: 60px;
-		height: 60px;
+	a {
+		color: var(--darkblue);
 	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
 	a:hover {
-		color: var(--color-theme-1);
+		color: var(--darkblue);
 	}
 </style>
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -13,17 +13,23 @@ const config = {
 			colors: {
 				// flowbite-svelte
 				primary: {
-					50: "#FFF5F2",
-					100: "#FFF1EE",
-					200: "#FFE4DE",
-					300: "#FFD5CC",
-					400: "#FFBCAD",
-					500: "#FE795D",
-					600: "#EF562F",
-					700: "#EB4F27",
-					800: "#CC4522",
-					900: "#A5371B",
+					// TODO: actually interpolate lightness here
+					50: "hsla(205, 57%, 23%, 1)",
+					100: "hsla(205, 57%, 23%, 1)",
+					200: "hsla(205, 57%, 23%, 1)",
+					300: "hsla(205, 57%, 23%, 1)",
+					400: "hsla(205, 57%, 23%, 1)",
+					500: "hsla(205, 57%, 23%, 1)",
+					600: "hsla(205, 57%, 23%, 1)",
+					700: "hsla(205, 57%, 23%, 1)",
+					800: "hsla(205, 57%, 23%, 1)",
+					900: "hsla(205, 57%, 23%, 1)",
 				},
+				// then match the css global variables defined in .postcss file
+				darkblue: "hsla(205, 57%, 23%, 1)",
+				lightblue: "hsla(198, 41%, 54%, 1)",
+				darkorange: "hsla(27, 77%, 55%, 1)",
+				lightorange: "hsla(38, 83%, 60%, 1)",
 			},
 		},
 	},
