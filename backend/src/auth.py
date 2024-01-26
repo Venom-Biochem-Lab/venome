805916fc309cfc030a7a6679097958b23ce14743
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2024-01-25 22:35:20 -0800

    Changed expiration on token to 24 hours

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -9,12 +9,6 @@ def generateAuthToken(userId, admin):
     payload = {
         "email": userId,
         "admin": admin,
-        "exp": datetime.now(tz=timezone.utc) + timedelta(seconds=30)
+        "exp": datetime.now(tz=timezone.utc) + timedelta(hours=24)
     }
     return jwt.encode(payload, secret_key, algorithm="HS256")
-
-if __name__ == "__main__":
-    userId = "ansengarvin@gmail.com"
-    admin = True
-    token = generateAuthToken(userId, admin)
-    print(token)
