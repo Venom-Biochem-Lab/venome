b9315cafb8feaf2d74c2a0e6530e20c4685f4cf7
xnought <bertuccd@oregonstate.edu>
2024-01-29 19:47:20 -0800

    feat: single entry

--- frontend/src/lib/ListProteins.svelte
+++ frontend/src/lib/ListProteins.svelte
@@ -2,80 +2,82 @@
 	import { goto } from "$app/navigation";
 	import type { ProteinEntry } from "$lib/backend";
 	import { numberWithCommas } from "$lib/format";
-	import {
-		Table,
-		TableBody,
-		TableBodyCell,
-		TableBodyRow,
-		TableHead,
-		TableHeadCell,
-	} from "flowbite-svelte";
 	import { Tabs, TabItem } from "flowbite-svelte";
-	import { Card } from "flowbite-svelte";
+	import dummy from "$lib/images/dummy.png";
 
 	export let allEntries: ProteinEntry[] | null = null;
+	const dummyDesc =
+		"scriptionDescriptionDescription DescriptionDescription Description Description";
 </script>
 
-<Tabs style="underline" contentClass="bg-none p-5">
-	<TabItem open title="Table">
-		<Table>
-			<TableHead>
-				<TableHeadCell>Protein name</TableHeadCell>
-				<TableHeadCell>Organism</TableHeadCell>
-				<TableHeadCell>Length</TableHeadCell>
-				<TableHeadCell>Mass (Da)</TableHeadCell>
-			</TableHead>
-			<TableBody tableBodyClass="divide-y">
-				{#if allEntries}
-					{#each allEntries as entry}
-						<TableBodyRow
-							class="cursor-pointer hover:bg-gray-100"
-							on:click={() => {
-								goto(`/protein/${entry.name}`);
-							}}
-						>
-							<TableBodyCell
-								><span class="text-primary-700">{entry.name}</span
-								></TableBodyCell
-							>
-							<TableBodyCell>{entry.speciesName}</TableBodyCell>
-							<TableBodyCell>{entry.length}</TableBodyCell>
-							<TableBodyCell>{numberWithCommas(entry.mass)}</TableBodyCell>
-						</TableBodyRow>
-					{/each}
-				{/if}
-			</TableBody>
-		</Table>
-	</TabItem>
-	<TabItem title="Gallery">
-		<div class="entries">
-			{#if allEntries}
-				{#each allEntries as entry}
-					<Card
-						class="hover:shadow-lg cursor-pointer"
-						title="Click to see {entry.name}"
-						on:click={() => goto(`/protein/${entry.name}`)}
-					>
-						<div class="name text-primary-700">
+<Tabs style="full" contentClass="bg-none p-5">
+	<TabItem open title="Proteins">
+		{#if allEntries}
+			{#each allEntries as entry}
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<div
+					class="prot-container"
+					on:click={() => goto(`/protein/${entry.name}`)}
+				>
+					<div class="prot-thumb">
+						<img class="prot-thumb" src={dummy} alt="dummy" />
+					</div>
+					<div>
+						<div class="prot-name">
 							{entry.name}
 						</div>
-						<div class="description">
-							Length: {entry.length}, Mass (Da): {numberWithCommas(entry.mass)}
+						<div class="prot-desc">
+							{dummyDesc}
 						</div>
-					</Card>
-				{/each}
-			{/if}
-		</div>
+						<div class="mt-2">
+							<div>
+								<b>Organism</b>: {entry.speciesName},
+								<b>Method</b>: AlphaFold2
+							</div>
+							<div>
+								<b>Length</b>: <code>{entry.length}</code>,
+								<b>Mass</b>: <code>{numberWithCommas(entry.mass)}</code>
+							</div>
+						</div>
+					</div>
+				</div>
+			{/each}
+		{/if}
 	</TabItem>
+	<TabItem title="Scatter Plot">not yet implemented</TabItem>
 </Tabs>
 
 <style>
-	.entries {
+	.prot-container {
 		display: flex;
-		flex-wrap: wrap;
-		gap: 20px;
+		outline: hsla(var(--darkblue-hsl), 0.3) 1px solid;
+		border-radius: 5px;
+		width: 500px;
+		padding-left: 15px;
+		padding-bottom: 15px;
+		padding-top: 15px;
 	}
-	.name {
+	.prot-container:hover {
+		transform: scale(1.02);
+		transition: all 0.2s ease-in-out;
+		outline: var(--darkblue) 1px dashed;
+		cursor: pointer;
+	}
+	.prot-thumb {
+		width: 150px;
+		height: 150px;
+	}
+	.prot-name {
 		font-size: 1.5em;
+		color: var(--darkblue);
+	}
+	.prot-desc {
+		overflow-wrap: break-word;
+		width: 300px;
+		font-weight: 300;
+	}
+	b {
+		font-weight: 500;
 	}
 </style>
