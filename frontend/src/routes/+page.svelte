9f950bc03611b47463b6aeabbedf7507889a7702
xnought <bertuccd@oregonstate.edu>
2024-01-29 19:03:46 -0800

    feat: don't reroute home

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,19 +1,10 @@
 <script lang="ts">
 	import { goto } from "$app/navigation";
 	import { onMount } from "svelte";
-
-	// remove once we have a landing page
-	onMount(() => {
-		goto("/search");
-	});
 </script>
 
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 
-<div>Landing Page</div>
-
-<style>
-	/*  put stuff here */
-</style>
+<div>Home Page!</div>
