0c1065135d9b73d19bd92c95cb5df329f8a92825
xnought <bertuccd@oregonstate.edu>
2024-02-08 00:28:20 -0800

    feat: remove old frontend

--- frontend-old/src/lib/backend.ts
+++
@@ -1,8 +0,0 @@
-export * from "../openapi";
-export { DefaultService as Backend } from "../openapi";
-import { OpenAPI } from "../openapi";
-import { env } from "$env/dynamic/public";
-
-export const BACKEND_URL = env["PUBLIC_BACKEND_URL"];
-if (!BACKEND_URL) throw new Error("PUBLIC_BACKEND_URL is not set in .env");
-OpenAPI.BASE = BACKEND_URL;
--- frontend-old/src/routes/edit/[proteinName]/+page.ts
+++
@@ -1,7 +0,0 @@
-/**
- * This scrapes the data from the route
- * ie. /edit/your_mom -> { proteinName: "your_mom" }
- */
-export function load({ params }) {
-	return { proteinName: params.proteinName };
-}
