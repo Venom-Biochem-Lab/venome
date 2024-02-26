a308e7170ebc3eca3ddcda0eec35951826ebbb04
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2024-02-25 20:27:20 -0800

    Added bearer token checking to restricted API calls - Protein upload, delete, and edit.

--- backend/src/auth.py
+++ backend/src/auth.py
@@ -1,5 +1,7 @@
 import jwt
 from datetime import datetime, timezone, timedelta
+from fastapi.requests import Request
+from fastapi import HTTPException
 
 # TODO: This method of secret key generation is, obviously, extremely unsafe.
 # This needs to be changed.
@@ -15,13 +17,31 @@ def generateAuthToken(userId, admin):
     return jwt.encode(payload, secret_key, algorithm="HS256")
 
 
-# TODO: Find out how FastAPI handles middleware functions, and turn this into one.
 def authenticateToken(token):
     # Return the decoded token if it's valid.
     try:
-        decoded = jwt.decode(token, secret_key, algorithms="HS256")
+        # Valid token is always is in the form "Bearer [token]", so we need to slice off the "Bearer" portion.
+        sliced_token = token[7:]
+        print(sliced_token)
+        decoded = jwt.decode(sliced_token, secret_key, algorithms="HS256")
+        print("Valid token")
+        print(decoded)
         return decoded
 
     # If the token is invalid, return None.
-    except Exception:
+    except Exception as err:
+        print("Invalid token:", err)
         return None
+
+
+# Use this function with a request if you want.
+def requiresAuthentication(req: Request):
+    userInfo = authenticateToken(req.headers["authorization"])
+    if (not userInfo or not userInfo.get("admin")):
+        print("Unauthorized User")
+        raise HTTPException(
+            status_code=403,
+            detail="Unauthorized"
+        )
+    else:
+        print("User authorized.")
