c2aec04651adf5cfdb04d12a66e8ca989b22c8d5
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2024-03-10 10:54:36 -0700

    Format: Commented out stdout debug print

--- backend/src/tmalign.py
+++ backend/src/tmalign.py
@@ -65,7 +65,7 @@ def tm_align(
         try:
             bash_cmd(f"mkdir {full_path}")
             log.warn(f"Attempting to align now with cmd {cmd}")
-            stdout = bash_cmd(cmd)
+            # stdout = bash_cmd(cmd)
             # log.warn(stdout)
 
         except Exception as e:
