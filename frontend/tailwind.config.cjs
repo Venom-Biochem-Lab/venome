58a13a72998c6f2056073e91c286b852c4e0c5bd
xnought <bertuccd@oregonstate.edu>
2024-04-02 19:31:25 -0700

    feat: tailwind range

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -11,21 +11,20 @@ const config = {
 	theme: {
 		extend: {
 			colors: {
-				// flowbite-svelte
 				primary: {
-					// TODO: actually interpolate lightness here
-					50: "hsla(205, 57%, 23%, 1)",
-					100: "hsla(205, 57%, 23%, 1)",
-					200: "hsla(205, 57%, 23%, 1)",
-					300: "hsla(205, 57%, 23%, 1)",
-					400: "hsla(205, 57%, 23%, 1)",
-					500: "hsla(205, 57%, 23%, 1)",
-					600: "hsla(205, 57%, 23%, 1)",
-					700: "hsla(205, 57%, 23%, 1)",
-					800: "hsla(205, 57%, 23%, 1)",
-					900: "hsla(205, 57%, 23%, 1)",
+					50: "#f2f8fd",
+					100: "#e4f0fa",
+					200: "#c4e1f3",
+					300: "#8fc8ea",
+					400: "#54abdc",
+					500: "#2d91ca",
+					600: "#1e74ab",
+					700: "#194f73", // flowbite uses this one for 'primary' variants
+					800: "#194f73",
+					900: "#19405c", // same as darkblue below
+					950: "#112b40",
 				},
-				// then match the css global variables defined in .postcss file
+				// then match the css global variables defined in .postcss file so these can be used in tailwind
 				darkblue: "hsla(205, 57%, 23%, 1)",
 				lightblue: "hsla(198, 41%, 54%, 1)",
 				darkorange: "hsla(27, 77%, 55%, 1)",
