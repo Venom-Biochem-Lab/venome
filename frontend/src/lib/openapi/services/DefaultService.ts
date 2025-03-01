d7f74374b868a15f277de1cd8c0cb3c6e0d288cc
arc25275 <arc25275@gmail.com>
2025-03-01 11:11:21 -0800

    feat(api): Add api calls to get a user for a protein, and proteins for a user

--- frontend/src/lib/openapi/services/DefaultService.ts
+++ frontend/src/lib/openapi/services/DefaultService.ts
@@ -140,8 +140,8 @@ export class DefaultService {
     ): CancelablePromise<UserResponse> {
         return __request(OpenAPI, {
             method: 'GET',
-            url: '/user/{id}',
-            query: {
+            url: '/user/{user_id}',
+            path: {
                 'user_id': userId,
             },
             errors: {
@@ -162,8 +162,8 @@ export class DefaultService {
     ): CancelablePromise<any> {
         return __request(OpenAPI, {
             method: 'PUT',
-            url: '/user/{id}',
-            query: {
+            url: '/user/{user_id}',
+            path: {
                 'user_id': userId,
             },
             body: requestBody,
@@ -184,8 +184,28 @@ export class DefaultService {
     ): CancelablePromise<any> {
         return __request(OpenAPI, {
             method: 'DELETE',
-            url: '/user/{id}',
-            query: {
+            url: '/user/{user_id}',
+            path: {
+                'user_id': userId,
+            },
+            errors: {
+                422: `Validation Error`,
+            },
+        });
+    }
+    /**
+     * Get User Proteins
+     * @param userId
+     * @returns any Successful Response
+     * @throws ApiError
+     */
+    public static getUserProteins(
+        userId: number,
+    ): CancelablePromise<(Array<string> | null)> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/user/{user_id}/proteins',
+            path: {
                 'user_id': userId,
             },
             errors: {
@@ -382,6 +402,26 @@ export class DefaultService {
         return __request(OpenAPI, {
             method: 'DELETE',
             url: '/protein/entry/{protein_name}',
+            path: {
+                'protein_name': proteinName,
+            },
+            errors: {
+                422: `Validation Error`,
+            },
+        });
+    }
+    /**
+     * Get Protein Entry User
+     * @param proteinName
+     * @returns UserResponse Successful Response
+     * @throws ApiError
+     */
+    public static getProteinEntryUser(
+        proteinName: string,
+    ): CancelablePromise<UserResponse> {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/protein/entry/{protein_name}/user',
             path: {
                 'protein_name': proteinName,
             },
